2840b2b4d53ddf6903c700683cc41efd307c3d0a
amar0026 <amarjeetnapit1@gmail.com>
2026-03-10 18:17:37 +0530

    about section add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,13 @@ import DealsSection from '../components/home/DealsSection';
 import BrowseByCategory from '../components/home/BrowseByCategory';
 import MusicExperienceShowcase from '../components/home/MusicExperienceShowcase';
 import ExploreProducts from '../components/home/ExploreProducts';
+import AboutPage from '../components/about';
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen">
       <HeroSection />
+      <AboutPage/>
       <DealsSection />
       <BrowseByCategory />
       <MusicExperienceShowcase />
