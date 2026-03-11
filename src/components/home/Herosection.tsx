24f464c6038e81ad5350ba6db29128e51af266a0
amar0026 <amarjeetnapit1@gmail.com>
2026-03-11 17:43:22 +0530

    some errors fix

--- src/components/home/Herosection.tsx
+++ src/components/home/Herosection.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface Slide {
